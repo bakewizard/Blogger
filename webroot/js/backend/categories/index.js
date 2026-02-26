b74ad2e62613a04c14170a12d1b457ffda01a69d
bakewizard <207716455+bakewizard@users.noreply.github.com>
2026-02-26 16:06:36 +0200

    fix

--- webroot/js/backend/categories/index.js
+++ webroot/js/backend/categories/index.js
@@ -1,19 +1,12 @@
-{
-    "use strict";
+const form = document.getElementById('index-form');
 
-    new Sortable(document.getElementById('categories'), {
-        handle: '.drag-handle', // handle's class
-        animation: 150,
-        onEnd: sort
-    });
-
-    function sort(e) {
-        let form = document.getElementById('index-form');
+new BakeKit.ui.Sortable(document.getElementById('categories'), {
+    handle: '.drag-handle',
+    animation: 150,
+    onEnd(e) {
         form['id'].value = e.item.dataset.id;
-        form['oldIndex'].value = (e.oldIndex + 1);
-        form['newIndex'].value = (e.newIndex + 1);
-
+        form['oldIndex'].value = e.oldIndex + 1;
+        form['newIndex'].value = e.newIndex + 1;
         form.submit();
     }
-
-}
+});
